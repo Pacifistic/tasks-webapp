b8e6f54b9b5cd94a80b441a14faa2f91b40e8f57
Dominykas Sipelis <DominykasSipelis@gmail.com>
2021-08-17 10:12:06 -0500

    Initial commit

---
+++ src/main/java/com/redarmdevs/taskswebapp/TasksWebappApplication.java
@@ -0,0 +1,13 @@
+package com.redarmdevs.taskswebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TasksWebappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/redarmdevs/taskswebapp/TasksWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redarmdevs.taskswebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
